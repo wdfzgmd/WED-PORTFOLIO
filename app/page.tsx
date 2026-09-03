818e70264f9f89d12f4bc196dc3b5c90f84a95af
Wed Fawaz <alsoraci@gmail.com>
2026-09-03 16:52:42 +0300

    Add Skills section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <Skills />
     </main>
   );
 }
